39c1c3af2bfdba78f4971df944c8c41a8f40cb87
Darren Kopp <darrenkopp@gmail.com>
2012-04-17 20:36:34 -0600

    initial commit

---
+++ DynamicInvestigation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TypeLibrary;
+
+namespace DynamicInvestigation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic nonPublicThisAssembly = Activator.CreateInstance(typeof(NonPublicType));
+            dynamic publicThisAssembly = Activator.CreateInstance(typeof(PublicType));
+            dynamic nonPublicOtherAssembly = Activator.CreateInstance(Type.GetType("TypeLibrary.NonPublicTypeOtherAssembly, TypeLibrary"));
+            dynamic publicOtherAssembly = Activator.CreateInstance(typeof(PublicTypeOtherAssembly));
+
+            PrintType(nonPublicThisAssembly);
+            PrintType(publicThisAssembly);
+            PrintType(nonPublicOtherAssembly);
+            PrintType(publicOtherAssembly);
+            Console.Read();
+        }
+
+        static void PrintType<T>(T instance)
+        {
+            Console.WriteLine(typeof(T));
+        }
+    }
+}
---
+++ DynamicInvestigation/PublicType.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DynamicInvestigation
+{
+    public class PublicType
+    {
+    }
+}
